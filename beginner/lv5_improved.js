1cec05f2cb05c735a4664e51704b9f4539252273
uahnbu <uahnburn@gmail.com>
2017-05-07 11:00:59 +0800

    Create lv5_improved.js

---
+++ beginner/lv5_improved.js
@@ -0,0 +1,17 @@
+var health = 0, heal = 0
+class Player {
+  playTurn(warrior) {
+    if (warrior.feel().isEmpty())
+      if (warrior.health() > 12 || health > warrior.health()) {
+        warrior.walk()
+        if (warrior.health() > 12 && heal == 1) heal = 2
+      } else if (heal < 2) {
+        warrior.rest()
+        heal = 1
+      } else warrior.walk()
+    else
+      if (warrior.feel().isCaptive()) warrior.rescue()
+      else warrior.attack()
+    health = warrior.health()
+  }
+}
